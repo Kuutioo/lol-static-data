88b55ed21fc09dca04784d523bba41ff1b808406
Arttu Hakkarainen <ape.hakkarainen@gmail.com>
2022-05-27 10:47:59 +0200

    Asset images

--- lol_static_data/lib/main.dart
+++ lol_static_data/lib/main.dart
@@ -7,11 +7,10 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'lol_static_data',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
