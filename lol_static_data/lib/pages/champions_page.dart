1f38714e2bee71ff9e7d236cf9d1e29c2479e0b4
Janu <ante.sarre@gmail.com>
2022-05-27 13:13:54 +0200

    Update champions_page.dart

--- lol_static_data/lib/pages/champions_page.dart
+++ lol_static_data/lib/pages/champions_page.dart
@@ -1,10 +1,36 @@
-// ignore_for_file: use_key_in_widget_constructors
-
 import 'package:flutter/material.dart';
 
 class ChampionsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Champions'),
+      ),
+      body: GridView.builder(
+        gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 150,
+          childAspectRatio: 1,
+          crossAxisSpacing: 5,
+          mainAxisSpacing: 5,
+        ),
+        itemCount: 140,
+        itemBuilder: ((context, index) {
+          return GridTile(
+            child: IconButton(
+              icon: Image.network(
+                'https://uning.es/wp-content/uploads/2016/08/ef3-placeholder-image.jpg',
+                fit: BoxFit.cover,
+              ),
+              onPressed: () {},
+            ),
+            footer: const Text(
+              'lol',
+              textAlign: TextAlign.center,
+            ),
+          );
+        }),
+      ),
+    );
   }
 }
