ea3b4557bd610a1d4a89cefd9fb26f975e10130a
Arttu Hakkarainen <ape.hakkarainen@gmail.com>
2022-06-01 09:37:47 +0200

    Update champion_tips.dart

--- lol_static_data/lib/widgets/champion_tips.dart
+++ lol_static_data/lib/widgets/champion_tips.dart
@@ -37,7 +37,6 @@ class ChampionTips extends StatelessWidget {
           return null;
         }
         if (snapshot.hasData) {
-          print('xd here');
           return _championTips(snapshot);
         }
         return null;
@@ -53,28 +52,28 @@ class ChampionTips extends StatelessWidget {
         const ChampionDetailText('Tips', 32, true),
         ChampionDetailText('Tips playing with ${champion.name}', 24, true),
         !snapshot.data[0].isEmpty
-            ? ChampionDetailText('- ${snapshot.data[0]}', 18, false)
+            ? ChampionDetailText('- ${snapshot.data[0][0]}', 18, false)
             : const SizedBox.shrink(),
         const SizedBox(height: 10),
         !snapshot.data[0].isEmpty
-            ? ChampionDetailText('- ${snapshot.data[0]}', 18, false)
+            ? ChampionDetailText('- ${snapshot.data[0][1]}', 18, false)
             : const SizedBox.shrink(),
         const SizedBox(height: 10),
         !snapshot.data[0].isEmpty
-            ? ChampionDetailText('- ${snapshot.data[0]}', 18, false)
+            ? ChampionDetailText('- ${snapshot.data[0][2]}', 18, false)
             : const SizedBox.shrink(),
         const SizedBox(height: 10),
         ChampionDetailText('Tips playing against ${champion.name}', 24, true),
         !snapshot.data[1].isEmpty
-            ? ChampionDetailText('- ${snapshot.data[1]}', 18, false)
+            ? ChampionDetailText('- ${snapshot.data[1][0]}', 18, false)
             : const SizedBox.shrink(),
         const SizedBox(height: 10),
         !snapshot.data[1].isEmpty
-            ? ChampionDetailText('- ${snapshot.data[1]}', 18, false)
+            ? ChampionDetailText('- ${snapshot.data[1][1]}', 18, false)
             : const SizedBox.shrink(),
         const SizedBox(height: 10),
         !snapshot.data[1].isEmpty
-            ? ChampionDetailText('- ${snapshot.data[1]}', 18, false)
+            ? ChampionDetailText('- ${snapshot.data[1][2]}', 18, false)
             : const SizedBox.shrink(),
       ],
     );
