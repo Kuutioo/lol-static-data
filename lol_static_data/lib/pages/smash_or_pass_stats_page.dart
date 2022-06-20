774dda198a59be5f1233a4925c6f2e779e4242b8
Arttu Hakkarainen <ape.hakkarainen@gmail.com>
2022-06-20 09:54:17 +0200

    Correct smash and pass count on statistics page

--- lol_static_data/lib/pages/smash_or_pass_stats_page.dart
+++ lol_static_data/lib/pages/smash_or_pass_stats_page.dart
@@ -4,6 +4,7 @@ import 'package:lol_static_data/helpers/gradient_text.dart';
 import 'package:lol_static_data/main.dart';
 import 'package:new_gradient_app_bar/new_gradient_app_bar.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 import 'package:lol_static_data/widgets/hamburger_bar.dart';
 
@@ -17,108 +18,141 @@ class SmashOrPassStatsPage extends StatelessWidget {
     String times = AppLocalizations.of(context).times;
     String stats = AppLocalizations.of(context).stats;
 
-    return Scaffold(
-      drawer: HamburgerBar(),
-      appBar: NewGradientAppBar(
-        actions: [
-          IconButton(
-            onPressed: () {},
-            icon: Icon(Icons.filter_alt_rounded),
-          )
-        ],
-        gradient: const LinearGradient(
-          begin: Alignment.centerLeft,
-          end: Alignment.centerRight,
-          colors: [
-            Color.fromARGB(255, 47, 69, 76),
-            Color.fromARGB(255, 7, 32, 44),
-          ],
-        ),
-        elevation: 0,
-        iconTheme: IconThemeData(
-          color: Colors.white,
-          size: MediaQuery.of(context).size.width > 700 ? 40 : 24,
-        ),
-        title: Text('$smashOrPass $stats'),
-      ),
-      body: Container(
-        decoration: const BoxDecoration(
-          gradient: LinearGradient(
-            begin: Alignment.centerLeft,
-            end: Alignment.centerRight,
-            colors: [
-              Color.fromARGB(255, 47, 69, 76),
-              Color.fromARGB(255, 7, 32, 44),
+    return FutureBuilder(
+      future: FirebaseFirestore.instance.collection('champions').get(),
+      builder: (context, AsyncSnapshot<QuerySnapshot> streamSnapshot) {
+        if (streamSnapshot.connectionState == ConnectionState.waiting) {
+          return Container(
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.bottomCenter,
+                end: Alignment.topCenter,
+                colors: [
+                  Color.fromARGB(255, 47, 69, 76),
+                  Color.fromARGB(255, 7, 32, 44),
+                ],
+              ),
+            ),
+            child: const Center(
+              child: CircularProgressIndicator(),
+            ),
+          );
+        }
+        if (streamSnapshot.hasError) {
+          print(streamSnapshot.error);
+          return Center(
+            child: Text('Error: ${streamSnapshot.error}'),
+          );
+        }
+        var documents = streamSnapshot.data.docs;
+
+        return Scaffold(
+          drawer: HamburgerBar(),
+          appBar: NewGradientAppBar(
+            actions: [
+              IconButton(
+                onPressed: () {},
+                icon: Icon(Icons.filter_alt_rounded),
+              )
             ],
+            gradient: const LinearGradient(
+              begin: Alignment.centerLeft,
+              end: Alignment.centerRight,
+              colors: [
+                Color.fromARGB(255, 47, 69, 76),
+                Color.fromARGB(255, 7, 32, 44),
+              ],
+            ),
+            elevation: 0,
+            iconTheme: IconThemeData(
+              color: Colors.white,
+              size: MediaQuery.of(context).size.width > 700 ? 40 : 24,
+            ),
+            title: Text('$smashOrPass $stats'),
           ),
-        ),
-        child: ListView.builder(
-          itemCount: championList.length,
-          itemBuilder: (context, index) {
-            return Container(
-              key: UniqueKey(),
-              child: Padding(
-                padding: EdgeInsets.only(
-                  left: 10,
-                  bottom: 10,
-                ),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    GradientText(
-                      championList[index].name,
-                      gradient: const LinearGradient(
-                        begin: Alignment.topCenter,
-                        end: Alignment.bottomCenter,
-                        colors: [
-                          Color.fromARGB(255, 171, 150, 76),
-                          Color.fromARGB(255, 247, 217, 110),
-                        ],
-                      ),
-                      style:
-                          TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+          body: Container(
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.centerLeft,
+                end: Alignment.centerRight,
+                colors: [
+                  Color.fromARGB(255, 47, 69, 76),
+                  Color.fromARGB(255, 7, 32, 44),
+                ],
+              ),
+            ),
+            child: ListView.builder(
+              itemCount: championList.length,
+              itemBuilder: (context, index) {
+                int smashCount = documents[index]['smash_count'];
+                int passCount = documents[index]['pass_count'];
+                return Container(
+                  key: UniqueKey(),
+                  child: Padding(
+                    padding: EdgeInsets.only(
+                      left: 10,
+                      bottom: 10,
                     ),
-                    Row(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        Container(
-                          width: 75,
-                          height: 75,
-                          decoration: BoxDecoration(
-                            image: DecorationImage(
-                              image: NetworkImage(championList[index].icon.url),
-                              fit: BoxFit.fill,
-                            ),
+                        GradientText(
+                          championList[index].name,
+                          gradient: const LinearGradient(
+                            begin: Alignment.topCenter,
+                            end: Alignment.bottomCenter,
+                            colors: [
+                              Color.fromARGB(255, 171, 150, 76),
+                              Color.fromARGB(255, 247, 217, 110),
+                            ],
                           ),
+                          style: TextStyle(
+                              fontSize: 24, fontWeight: FontWeight.bold),
                         ),
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        Row(
                           children: [
-                            Text(
-                              '$smashed: 10 $times',
-                              style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 18,
+                            Container(
+                              width: 75,
+                              height: 75,
+                              decoration: BoxDecoration(
+                                image: DecorationImage(
+                                  image: NetworkImage(
+                                      championList[index].icon.url),
+                                  fit: BoxFit.fill,
+                                ),
                               ),
                             ),
-                            Text(
-                              '$passed: 15 $times',
-                              style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 18,
-                              ),
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Text(
+                                  '$smashed: $smashCount $times',
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 18,
+                                  ),
+                                ),
+                                Text(
+                                  '$passed: $passCount $times',
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 18,
+                                  ),
+                                ),
+                              ],
                             ),
                           ],
-                        ),
+                        )
                       ],
-                    )
-                  ],
-                ),
-              ),
-            );
-          },
-        ),
-      ),
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
+        );
+      },
     );
   }
 }
