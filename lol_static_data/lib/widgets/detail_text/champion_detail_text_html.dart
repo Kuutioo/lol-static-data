8cc390815375210193bf99047de80f650a1e284f
Janu <ante.sarre@gmail.com>
2022-06-22 13:37:02 +0200

    html text animation

--- lol_static_data/lib/widgets/detail_text/champion_detail_text_html.dart
+++ lol_static_data/lib/widgets/detail_text/champion_detail_text_html.dart
@@ -2,6 +2,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_html/flutter_html.dart';
+import 'package:animated_widgets/animated_widgets.dart';
 
 class ChampionDetailTextHtml extends StatelessWidget {
   final String text;
@@ -11,17 +12,23 @@ class ChampionDetailTextHtml extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Html(
-      data: text,
-      shrinkWrap: false,
-      style: {
-        'body': Style(
-          fontSize: FontSize(fontSize),
-          color: Colors.white,
-          margin: const EdgeInsets.all(0),
-          padding: const EdgeInsets.only(left: 5),
-        ),
-      },
+    return TranslationAnimatedWidget.tween(
+      duration: Duration(milliseconds: 500),
+      translationDisabled: Offset(200, 0),
+      translationEnabled: Offset(0, 0),
+      enabled: true,
+      child: Html(
+        data: text,
+        shrinkWrap: false,
+        style: {
+          'body': Style(
+            fontSize: FontSize(fontSize),
+            color: Colors.white,
+            margin: const EdgeInsets.all(0),
+            padding: const EdgeInsets.only(left: 5),
+          ),
+        },
+      ),
     );
   }
 }
